56fefae9982b59b2fc231b732ab3d08db549ccb5
Filipe Gaio <filipe.gaio@hotmail.com>
2025-03-09 11:47:07 +0000

    omp for parelelisation implemented. might need adjustments script adjustment

--- run.sh
+++ run.sh
@@ -1,20 +1,22 @@
 # Remove old binary
-rm -f fabiosa
+rm -f file
+rm -f omp
 
 # Compilation with warning, PAPI and otimization flags
-g++ -Wall -o fabiosa fabiosa.cpp -O2 -lpapi
+g++ -Wall -o file file.cpp -O2 -lpapi
+g++ -Wall -o omp omp.cpp -O2 -lpapi -fopenmp
 
 # Run tests set
-echo "Algorithm 3 - Matrix 10240"
+echo "Running tests"
 for attemp in {1..4}
 do
-    ./fabiosa "3" "1000" "20" "data/data.txt"
+    ./file "3" "1000" "20"  #still needs implementation so that file can receive arguments
 done
 
 for block_size in 256 512
 do
     for attemp in {1..5}
     do
-        ./fabiosa "3" "10240" "$block_size" "data/data.txt"
+        ./omp "3" "1000" "$block_size" #still needs implementation so that file can receive arguments
     done
 done
